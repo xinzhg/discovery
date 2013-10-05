8e147f295edf601d1175dd7daf9ba3d960afcb5c
xinnan.zx <xinnan.zx@taobao.com>
2013-10-05 23:23:17 +0800

    add http response result dataobject

---
+++ biz/src/main/java/com/github/conanzhangxin/discovery/dataobject/HttpResult.java
@@ -0,0 +1,11 @@
+package com.github.conanzhangxin.discovery.dataobject;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xinnan.zx
+ * Date: 13-10-5
+ * Time: обнГ11:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class HttpResult {
+}
