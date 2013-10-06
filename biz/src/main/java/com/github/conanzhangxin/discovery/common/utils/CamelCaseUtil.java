9fda1563eff9fc64869df468f052252d88dc6018
xinnan.zx <xinnan.zx@taobao.com>
2013-10-06 23:09:10 +0800

    add camelcaseutil class

---
+++ biz/src/main/java/com/github/conanzhangxin/discovery/common/utils/CamelCaseUtil.java
@@ -0,0 +1,77 @@
+package com.github.conanzhangxin.discovery.common.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xinnan.zx
+ * Date: 13-10-6
+ * Time: ÏÂÎç9:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class CamelCaseUtil {
+
+    public static String toUnderScoreCase(String s) {
+        if (s == null) {
+            return null;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean upperCase = false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            boolean nextUpperCase = true;
+
+            if (i < (s.length() - 1)) {
+                nextUpperCase = Character.isUpperCase(s.charAt(i + 1));
+            }
+
+            if ((i > 0) && Character.isUpperCase(c)) {
+                if (!upperCase || !nextUpperCase) {
+                    sb.append(SEPARATOR);
+                }
+                upperCase = true;
+            } else {
+                upperCase = false;
+            }
+
+            sb.append(Character.toLowerCase(c));
+        }
+
+        return sb.toString();
+    }
+
+    private static final char SEPARATOR = '_';
+
+    public static String toCamelCase(String s) {
+        if (s == null) {
+            return null;
+        }
+
+        s = s.toLowerCase();
+
+        StringBuilder sb = new StringBuilder(s.length());
+        boolean upperCase = false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == SEPARATOR) {
+                upperCase = true;
+            } else if (upperCase) {
+                sb.append(Character.toUpperCase(c));
+                upperCase = false;
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static String toCapitalizeCamelCase(String s) {
+        if (s == null) {
+            return null;
+        }
+        s = toCamelCase(s);
+        return s.substring(0, 1).toUpperCase() + s.substring(1);
+    }
+}
